139c039ffd6aa69d28824f96e9e581fca5e9d1eb
sukjuhong <ehensnfl@gmail.com>
2024-06-28 05:18:40 +0900

    feat: express set trust proxy on production

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,9 @@ import {
 } from "./socket";
 
 const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
 
 const corsOption: cors.CorsOptions = {
   origin: "https://kwitch.online",
