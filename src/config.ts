ca5fd131bab30e787235066a2021ebd7795c1b64
Hong SukJu <ehensnfl@gmail.com>
2024-09-25 15:10:44 +0900

    feat: port 범위 설정 추가

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,107 @@
 import * as mediasoup from "mediasoup";
 
 export const config: {
-  app: {
-    port: number;
+  https: {
+    listenPort: string | number;
   };
   mediasoup: {
-    mediaCodecs: mediasoup.types.RtpCodecCapability[];
+    routerOptions: mediasoup.types.RouterOptions;
     transportOptions: mediasoup.types.WebRtcTransportOptions;
+    webRtcServerOptions: mediasoup.types.WebRtcServerOptions;
   };
 } = {
-  app: { port: parseInt(process.env.PORT || "8000", 10) },
+  https: {
+    listenPort: process.env.PORT || 8000,
+  },
   mediasoup: {
-    mediaCodecs: [
-      {
-        kind: "audio",
-        mimeType: "audio/opus",
-        clockRate: 48000,
-        channels: 2,
-      },
-      {
-        kind: "video",
-        mimeType: "video/VP8",
-        clockRate: 90000,
-        parameters: {
-          "x-google-start-bitrate": 1000,
-        },
-      },
-    ],
-    transportOptions: {
-      listenIps: [
+    routerOptions: {
+      mediaCodecs: [
         {
-          ip: process.env.LISTEN_IP || "127.0.0.1",
-          announcedIp: process.env.LISTEN_ANNOUNCED_IP || null,
+          kind: "audio",
+          mimeType: "audio/opus",
+          clockRate: 48000,
+          channels: 2,
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          parameters: {
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP9",
+          clockRate: 90000,
+          parameters: {
+            "profile-id": 2,
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "4d0032",
+            "level-asymmetry-allowed": 1,
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "42e01f",
+            "level-asymmetry-allowed": 1,
+            "x-google-start-bitrate": 1000,
+          },
         },
       ],
-      enableUdp: true,
-      enableTcp: true,
-      preferUdp: true,
+    },
+    webRtcServerOptions: {
+      listenInfos: [
+        {
+          protocol: "udp",
+          ip: process.env.MEDIASOUP_LISTEN_IP || "0.0.0.0",
+          announcedAddress: process.env.MEDIASOUP_ANNOUNCED_IP,
+          port: 44444,
+        },
+        {
+          protocol: "tcp",
+          ip: process.env.MEDIASOUP_LISTEN_IP || "0.0.0.0",
+          announcedAddress: process.env.MEDIASOUP_ANNOUNCED_IP,
+          port: 44444,
+        },
+      ],
+    },
+    transportOptions: {
+      listenInfos: [
+        {
+          protocol: "udp",
+          ip: "0.0.0.0",
+          announcedAddress: process.env.MEDIASOUP_ANNOUNCED_IP,
+          port: 40000,
+          portRange: {
+            min: parseInt(process.env.MEDIASOUP_MIN_PORT, 10) || 40000,
+            max: parseInt(process.env.MEDIASOUP_MAX_PORT, 10) || 49999,
+          },
+        },
+        {
+          protocol: "tcp",
+          ip: "0.0.0.0",
+          announcedAddress: process.env.MEDIASOUP_ANNOUNCED_IP,
+          port: 40000,
+          portRange: {
+            min: parseInt(process.env.MEDIASOUP_MIN_PORT, 10) || 40000,
+            max: parseInt(process.env.MEDIASOUP_MAX_PORT, 10) || 49999,
+          },
+        },
+      ],
     },
   },
 };
